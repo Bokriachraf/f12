d6083b46b8491b1f2cd27487581fa91072b96d93
unknown <achrafbokri@gmail.com>
2025-03-21 22:48:56 +0100

    v4

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,6 +1,12 @@
-import {PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL} from '../constants/productConstants'
+import {PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL, 
+    PRODUCT_DETAILS_FAIL,     
+    PRODUCT_DETAILS_SUCCESS,
+     PRODUCT_DETAILS_REQUEST} 
+     from '../constants/productConstants'
 
-export const productListReducer =(state={ products: [] },action)=>{
+export const productListReducer =(state={ loading: true, products: [] },action)=>{
 switch(action.type){
     case PRODUCT_LIST_REQUEST:
         return {loading:true};
@@ -12,3 +18,15 @@ switch(action.type){
                     return state
 }
 }
+export const productDetailsReducer =(state={ loading: true, products: [] },action)=>{
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return {loading:true};
+            case PRODUCT_DETAILS_SUCCESS:
+                return {loading:false, products: action.payload};
+                case PRODUCT_DETAILS_FAIL:
+                    return {loading:false, error: action.payload};
+                    default:
+                        return state
+    }
+    }
