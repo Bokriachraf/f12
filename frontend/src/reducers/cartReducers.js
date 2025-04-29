4f9a0602f3db73e2c03d17aa6f8e835982b824a5
unknown <achrafbokri@gmail.com>
2025-04-29 19:01:01 +0200

    createOrder

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,5 @@
 import { CART_ADD_ITEM, CART_REMOVE_ITEM, 
+  CART_EMPTY,
   CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
  } from '../constants/cartConstants';
@@ -31,7 +32,9 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           }
           case CART_SAVE_PAYMENT_METHOD:
             return { ...state, paymentMethod: action.payload };
-    default:
+          case CART_EMPTY:
+              return { ...state, cartItems: [] };
+            default:
       return state;
   }
 };
